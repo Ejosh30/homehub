d5ed15a687a1499e6a04541b3a06841178f1d479
i-AZUMA <richy@i-AZUMA-MacBook.local>
2026-07-01 21:16:51 +0100

    add property system

--- server.js
+++ server.js
@@ -7,65 +7,75 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-// Show your website
 app.use(express.static(path.join(__dirname)));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
 
-
-// MongoDB connection
+// DATABASE
 mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-  console.log("MongoDB connected");
+.then(()=>{
+console.log("MongoDB connected");
 })
-.catch((err) => {
-  console.log("MongoDB error:", err.message);
+.catch(err=>{
+console.log(err);
 });
 
 
-// Test route
-app.get("/api", (req, res) => {
-  res.json({
-    message: "HomeHub backend is working"
-  });
-});
+// PROPERTY MODEL
+const Property = mongoose.model("Property", {
 
-
-// Temporary property storage
-let properties = [];
-
-
-// Add property
-app.post("/api/properties", (req, res) => {
-
-  const property = {
-    id: Date.now(),
-    ...req.body
-  };
-
-  properties.push(property);
-
-  res.json({
-    message: "Property added",
-    property
-  });
+title:String,
+type:String,
+state:String,
+location:String,
+price:String,
+description:String,
+image:String
 
 });
 
 
-// Get properties
-app.get("/api/properties", (req,res)=>{
+// HOME PAGE
+app.get("/",(req,res)=>{
+res.sendFile(path.join(__dirname,"index.html"));
+});
 
-  res.json(properties);
+
+// ADD PROPERTY
+app.post("/api/properties", async(req,res)=>{
+
+try{
+
+const property = await Property.create(req.body);
+
+res.json({
+message:"Property listed successfully",
+property
+});
+
+}catch(error){
+
+res.status(500).json({
+error:error.message
+});
+
+}
+
+});
+
+
+// GET PROPERTIES
+app.get("/api/properties", async(req,res)=>{
+
+const properties = await Property.find();
+
+res.json(properties);
 
 });
 
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("HomeHub server running");
+
+app.listen(PORT,()=>{
+console.log("HomeHub server running");
 });
